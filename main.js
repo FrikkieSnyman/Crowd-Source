9d88cb8fcb229bec637a8e86808e7b74a7e3824c
Hanrich <Hanrichp@gmail.com>
2015-05-20 12:05:54 +0200

    Sending test data from server to client

--- main.js
+++ main.js
@@ -26,6 +26,12 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/names',function(request,response){
+	console.log("Client asked me for information");
+	var person = {name:"Hanrich"};
+	response.send(person);
+});
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
--- www/app.js
+++ www/app.js
@@ -1,6 +1,15 @@
+
+// This is our main module.
+// Here we will help cahnge stuff
 var app = angular.module('main', []);
-app.controller('mainCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+app.controller('mainCtrl', function($scope,$http) {
+    //$scope.firstName = "John";
+    //$scope.lastName = "Doe";
     $scope.name = "Piet";
+    $http.get("/names").success(function(response)
+    {
+    	console.log("We had a response");
+    	$scope.name = response.name;
+    	console.log(response);
+    });
 });
