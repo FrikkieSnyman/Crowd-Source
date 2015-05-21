7bb4861d956cfe3dd68693ab31951556ebb569ae
FrikkieSnyman <firkkie.snyman@live.co.za>
2015-05-21 11:11:55 +0200

    Failing a test to see how travis reacts

--- test.js
+++ test.js
@@ -1,14 +1,25 @@
 var main = require('./square.js');
 
 
-exports.testSomething = function(test){
+exports.testSquare = function(test){
     test.expect(1);
     var expected = 25;
     var result; 
     main.square(5,function(res){
       result = res;
     });
-    console.log(result);
     test.equal(expected,result);
     test.done();
 };
+
+exports.testSubtract = function(test){
+  test.expect(1);
+
+  var expected = 2;
+  var result;
+  main.subtract(4,2,function(res){
+    result = res;
+  });
+  test.equal(expected,result);
+  test.done();
+}
