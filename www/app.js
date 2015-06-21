7672a410164f0383763e9c4fe61d59bc21edba8c
ACalitz <andcalitz@gmail.com>
2015-06-21 18:38:00 +0200

    Fixed some layout issues

--- www/app.js
+++ www/app.js
@@ -2,33 +2,30 @@
 angular
 .module('main',['ngMaterial'])
 .controller('mainCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+	$scope.toggleLeft = buildToggler('left');
+ 	// $scope.toggleRight = buildToggler('right');
 
-  $scope.toggleLeft = buildToggler('left');
- // $scope.toggleRight = buildToggler('right');
+	/**
+	 * Build handler to open/close a SideNav; when animation finishes
+	 * report completion in console
+	 */
+	function buildToggler(navID) {
+		var debounceFn =  $mdUtil.debounce(function(){
+					$mdSidenav(navID)
+						.toggle()
+						.then(function () {
+							$log.debug("toggle " + navID + " is done");
+						});
+				},300);
 
-  /**
-   * Build handler to open/close a SideNav; when animation finishes
-   * report completion in console
-   */
-  function buildToggler(navID) {
-    var debounceFn =  $mdUtil.debounce(function(){
-          $mdSidenav(navID)
-            .toggle()
-            .then(function () {
-              $log.debug("toggle " + navID + " is done");
-            });
-        },300);
-
-    return debounceFn;
-  }
+		return debounceFn;
+	}
 })
 .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function () {
-    $mdSidenav('left').close()
-      .then(function () {
-        $log.debug("close LEFT is done");
-      });
-
-  };
+	$scope.close = function () {
+		$mdSidenav('left').close()
+			.then(function () {
+				$log.debug("close LEFT is done");
+			});
+	};
 });
-
