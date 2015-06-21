6e6b6402bddd055494b8fb37c7354a3ff685ce50
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-06-21 19:22:36 +0200

    Started work on project creation

--- tests/project.js
+++ tests/project.js
@@ -4,14 +4,38 @@ var testProject = process.env.CUSTOM_COV ?
 	require('../lib-cov/project/project.js')
 	: require('../lib/project/project.js');
 
-console.log('Running tests for demonstration purposes');
 exports.testCreateProject = function(test) {
-	test.expect(1);
-	var expected = 'This is a stub for creation of a new project';
+	test.expect(4);
+	var expected;
 	var result;
-	testProject.createProject(function(res) {
+
+	// Test 1: Test if correct exception is thrown when a non-string heading is sent
+	expected = 'Illegal heading format';
+	try {
+		testProject.createProject(null, 'test', function(res) {
+			result = res;
+		});
+	} catch (err) {
+		test.equal(err, expected);
+	}
+
+	// Test 2: Test if correct exception is thrown when a non-string description is sent
+	expected = 'Illegal description format';
+	try {
+		testProject.createProject('test', null, function(res) {
+			result = res;
+		});
+	} catch (err) {
+		test.equal(err, expected);
+	}
+
+	// Test 3: Test if a project is created and returned as an object succesfully
+	testProject.createProject('testProject', 'This is a test creation of a project', function(res) {
 		result = res;
-		test.equal(expected, result);
+		expected = 'testProject';
+		test.equal(expected, result.heading);
+		expected = 'This is a test creation of a project';
+		test.equal(expected, result.description);
 		test.done();
 	});
 };
@@ -43,6 +67,17 @@ exports.testUpdateProject = function(test) {
 	var expected = 'This is a stub for updating a project';
 	var result;
 	testProject.updateProject(function(res) {
+		result = res;
+		test.equal(expected, result);
+		test.done();
+	});
+};
+
+exports.testAddChild = function(test) {
+	test.expect(1);
+	var expected = 'temp';
+	var result;
+	testProject.addChild(result, function(res) {
 		result = res;
 		test.equal(expected, result);
 		test.done();
