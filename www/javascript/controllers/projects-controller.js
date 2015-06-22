b141c9031790afdb6dc0fe04f457c8eb4230faec
cbones1 <bones.greyvenstein@gmail.com>
2015-06-22 15:44:53 +0200

    List of projects implemented as cards

--- www/javascript/controllers/projects-controller.js
+++ www/javascript/controllers/projects-controller.js
@@ -1,7 +1,14 @@
 angular.module('main')
-.controller('projectsCtrl',['$scope','$http',function($scope,$http) {
+.controller('projectsCtrl',['$scope','$http', '$location',function($scope,$http, $location) {
 	$http({method:'GET',url:'/projects'}).success(function(data) {
 		$scope.projects = data;
 	});
+
+	$scope.createProject = function()
+	{
+		// console.log("Button press");
+		$location.path( "/createProject" );
+	}
+
 	// debugger;
 }]);
