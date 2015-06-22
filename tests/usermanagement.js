4bb6877d6c84c0bbbdd79e324e2d1ca6e1d45cc1
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-06-22 10:46:03 +0200

    User management unittest

---
+++ tests/usermanagement.js
@@ -0,0 +1,18 @@
+var mongoose = Object;
+var app = Object;
+mongoose.model = function(a, b) {};
+app.userSchema = function(a) {};
+
+require('./models.js')(app, mongoose);
+var testUM = process.env.CUSTOM_COV ?
+	require('../lib-cov/usermanagement/usermanagement.js')
+	: require('../lib/usermanagement/usermanagement.js')(app, mongoose);
+
+exports.testStuff = function(test) {
+	var request = Object;
+	request.body = {'name':'website','surname':'pew'};
+	var response = Object;
+	response.send = function(param) {};
+
+	testUM.addName(request, response);
+};
