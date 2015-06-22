74a248ca34b0298efcb516af7d08d0096394af30
Hanrich <Hanrichp@gmail.com>
2015-06-22 12:46:22 +0200

    Chenges to usermanagment

--- tests/usermanagement.js
+++ tests/usermanagement.js
@@ -1,9 +1,15 @@
 var mongoose = Object;
 var app = Object;
-mongoose.model = function(a, b) {};
-app.userSchema = function(a) {};
+mongoose.model = function(a, b) {
+	return function(){
+		this.name = '';
+		this.surname = '';
+		this.save = function(){};
+	}
+};
+app.userSchema = Object;
 
-require('./models.js')(app, mongoose);
+//require('./models.js')(app, mongoose);
 var testUM = process.env.CUSTOM_COV ?
 	require('../lib-cov/usermanagement/usermanagement.js')
 	: require('../lib/usermanagement/usermanagement.js')(app, mongoose);
@@ -15,4 +21,14 @@ exports.testStuff = function(test) {
 	response.send = function(param) {};
 
 	testUM.addName(request, response);
+
+		// Test 3: Test if a project is created and returned as an object succesfully
+	testProject.createProject('testAddName', 'This test the addNamefunction', function(res) {
+		result = res;
+		expected = 'testProject';
+		test.equal(expected, result.getHeading());
+		expected = 'This is a test creation of a project';
+		test.equal(expected, result.getDescription());
+		test.done();
+	});
 };
