0106e3e6b3ada554af0c862235775cc33a4d3f5d
Chris Cloete <u13029721@tuks.co.za>
2015-06-23 12:03:58 +0200

    Estimation page changes

--- www/javascript/controllers/estimation-controller.js
+++ www/javascript/controllers/estimation-controller.js
@@ -1,7 +1,12 @@
-angular.module('main')
-.controller('estimationCtrl', ['$scope', function($scope) {
-	$scope.estimation = {
-		project: '',
-		estimator: ''
-	};
-}]);
+(function() {
+	'use strict';
+	angular
+			.module('main')
+			.controller('estimationCtrl', estimators);
+	function estimators ($timeout, $q) {
+		var self = this;
+		self.name = null;
+		self.estimatorNames = ['Hugo', 'Andre', 'Frikkie'];
+		self.tags = [];
+	}
+})();
