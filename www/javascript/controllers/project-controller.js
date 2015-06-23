5f3718050ff3b81270e46968d418a0eec1702672
Hanrich <Hanrichp@gmail.com>
2015-06-23 08:38:10 +0200

    We can now build a tree for the project

--- www/javascript/controllers/project-controller.js
+++ www/javascript/controllers/project-controller.js
@@ -4,14 +4,6 @@ angular.module('main')
 	var project = {'heading': $routeParams.id};
 	$http({method:'POST', url:'/project', data: project}).success(function(data) {
 		$scope.project = data[0];
-		// Adding chlider to the project
-		$scope.project.children = [];
-		var childA = {name:'ChildA',description:'Child Description'};
-		var childB = {name:'ChildB',description:'Child Description'};
-		var childC = {name:'ChildC',description:'Child Description'};
-		$scope.project.children.push(childA);
-		$scope.project.children.push(childB);
-		$scope.project.children.push(childC);
-		//debugger;
+
 	});
 }]);
