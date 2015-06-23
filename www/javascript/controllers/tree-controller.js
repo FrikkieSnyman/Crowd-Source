cc2e70dfea43d19c981d5072854667f3bce44f32
Hanrich <Hanrichp@gmail.com>
2015-06-23 21:45:11 +0200

    We can now delete a node

--- www/javascript/controllers/tree-controller.js
+++ www/javascript/controllers/tree-controller.js
@@ -1,13 +1,33 @@
 angular.module('main').
 controller('TreeController', ['$scope', '$http', function($scope, $http) {
 	$scope.delete = function(data) {
-		data.nodes = [];
+		$scope.searchTree($scope.project.children, data.$$hashKey, function() {
+		});
 	};
 	$scope.add = function(data) {
 		var post = data.nodes.length + 1;
 		var newName = data.name + '-' + post;
 		data.nodes.push({name: newName, nodes: []});
 		$scope.project.children = $scope.tree;
-		$http({method:'POST', url:'/addChild', data: $scope.project});
+		$scope.save();
+	}
+	$scope.searchTree = function(node, id, callback) {
+		//console.log
+		var found = false;
+		for (var i in node) {
+			if (id === node[i].$$hashKey) {
+				found = true;
+				node.splice(i, 1);
+				$scope.save();
+			}
+		}
+		if (found === false) {
+			for (i in node) {
+				$scope.searchTree(node[i].nodes, id, function() {});
+			}
+		}
 	};
+	$scope.save = function() {
+		$http({method:'POST', url:'/addChild', data: $scope.project});
+	}
 }]);
