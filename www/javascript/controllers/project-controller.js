d9c2ae97b8df4f2bd6f7a2e124fcb7f4137ac110
Hanrich <Hanrichp@gmail.com>
2015-06-24 11:55:34 +0200

    Added a conformation dialog when you want to delete a node

--- www/javascript/controllers/project-controller.js
+++ www/javascript/controllers/project-controller.js
@@ -5,8 +5,23 @@ angular.module('main')
 	$http({method:'POST', url:'/project', data: project}).success(function(data) {
 		$scope.project = data[0];
 		$scope.tree = $scope.project.children;
+		$scope.rootIsEmpty = function() {
+		//debugger;
+		if (typeof $scope.project.children !== undefined) {
+			if ($scope.project.children.length < 1) {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
 	});
 	$scope.saveProject = function() {
-		$http({method:'POST', url:'/addChild', data: $scope.project});
+		$http({method:'POST', url:'/addChild', data: $scope.project}).success(function() {
+
+		});
+	}
+	$scope.addRootNode = function() {
+		$scope.project.children.push({name: "node", nodes: []});
 	}
 }]);
