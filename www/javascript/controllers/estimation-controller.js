e57db6942267627153e9cb0d1202225aeeb6f25e
Chris Cloete <u13029721@tuks.co.za>
2015-06-24 09:39:48 +0200

    Display changes to estimation

--- www/javascript/controllers/estimation-controller.js
+++ www/javascript/controllers/estimation-controller.js
@@ -7,8 +7,9 @@
 		//$http({method:'GET', url:'/projects'}).success(function(data) {
 		//this.projects = data;})
 		this.projectName = '';
-		this.projects = ('Project1 Project2 Project3 Project4').split(' ').map(function(name) { return {project: name}; });
+		this.projects = ['Project1', 'Project2', 'Project3', 'Project4'];
 		this.name = null;
 		this.estimatorNames = ['Hugo', 'Andre', 'Frikkie'];
+
 	}
 })();
