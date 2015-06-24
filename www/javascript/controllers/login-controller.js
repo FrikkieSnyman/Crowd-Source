631f698f22f7537dd22cb954b7974307bd3e212d
ACalitz <andcalitz@gmail.com>
2015-06-24 14:24:35 +0200

    Theme updates

--- www/javascript/controllers/login-controller.js
+++ www/javascript/controllers/login-controller.js
@@ -1,6 +1,23 @@
 angular.module('main')
 .controller('loginCtrl', ['$scope', '$location', '$window', '$mdDialog', 'userService', 'authenticationService',
 	function ($scope, $location, $window, $mdDialog, userService, authenticationService) {
+		$scope.logInOut = function(ev) {
+			if (!authenticationService.isLogged) {
+				showAdvanced(ev);
+			} else {
+				logout();
+			}
+		}
+
+		showAdvanced = function(ev) {
+			$mdDialog.show({
+				controller: DialogController,
+				templateUrl: '../../templates/pages/login/index.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+			});
+	  	};
+
 		$scope.logIn = function logIn(username, password) {
 			if (username !== undefined && password !== undefined) {
 				userService.logIn(username, password).
@@ -20,7 +37,7 @@ angular.module('main')
 			}
 		}
 
-		$scope.logout = function logout() {
+		logout = function logout() {
 			if (authenticationService.isLogged) {
 				authenticationService.isLogged = false;
 				delete $window.sessionStorage.token;
@@ -32,6 +49,15 @@ angular.module('main')
 
 		$scope.isLogged = function isLogged() {
 			return authenticationService.isLogged;
-
 		}
 }]);
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+	$mdDialog.hide();
+  };
+  $scope.cancel = function() {
+	$mdDialog.cancel();
+  };
+}
+
