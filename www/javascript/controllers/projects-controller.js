3c8c4cf0473848ed2fe7e2ddf2635f3cc88a8b6a
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-06-24 13:16:22 +0200

    Confirmation dialog for deleting a project

--- www/javascript/controllers/projects-controller.js
+++ www/javascript/controllers/projects-controller.js
@@ -1,5 +1,5 @@
 angular.module('main')
-.controller('projectsCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+.controller('projectsCtrl', ['$scope', '$http', '$location', '$mdDialog', function($scope, $http, $location, $mdDialog) {
 	$http({method:'GET', url:'/projects'}).success(function(data) {
 		$scope.projects = data;
 	});
@@ -10,9 +10,21 @@ angular.module('main')
 	};
 
 	$scope.deleteProject = function(project) {
-		$http({method:'POST', url:'/deleteProject', data: project}).success(function() {
-			$scope.updateProjects();
+
+		var confirm = $mdDialog.confirm()
+		.parent(angular.element(document.body))
+		.title('Are you sure you wish to delete project ' + project.name + '?')
+		.content('This will delete the project and prevent it from showing up in the list of projects.')
+		.ariaLabel('Delete the project')
+		.ok('Yes')
+		.cancel('No');
+		$mdDialog.show(confirm).then(function() {
+			$http({method:'POST', url:'/deleteProject', data: project}).success(function() {
+				$scope.updateProjects();
+			});
+		}, function() {
 		});
+
 	};
 
 	$scope.updateProjects = function() {
