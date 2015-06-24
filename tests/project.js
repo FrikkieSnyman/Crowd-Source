b17fe2be423021460e2e53b6d654838acdbd351d
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-06-24 12:08:01 +0200

    Tests are failing, but thats okay

--- tests/project.js
+++ tests/project.js
@@ -4,11 +4,13 @@ var mongoose = Object;
 var app = Object;
 mongoose.model = function(a, b) {
 	return function() {
+		this.name = '',
+		this.description = '',
 		this.save = function() {};
 		this.find = function() {};
 	};
 };
-app.projectSchema = Object;
+app.projectSchema = mongoose.model;
 var request = Object;
 var response = Object;
 response.send = function(param) {};
@@ -41,6 +43,7 @@ exports.testCreateProject = function(test) {
 };
 
 exports.testPersistProject = function(test) {
+	test.expect(2);
 	var Project = mongoose.model('Project', app.projectSchema);
 	var project = new Project({
 		name : 'test',
@@ -48,6 +51,15 @@ exports.testPersistProject = function(test) {
 		children : [{name : 'heading', nodes : []}]
 	});
 	testProject.persistProject(project);
+
+	expected = 'test';
+	result = project.name;
+	console.log(project);
+	test.equal(expected, result);
+
+	expected = 'description';
+	result = project.description;
+	test.equal(expected, result);
 
 	test.done();
 };
