a10b2cbe7ed064c1a0f72266d807333f936c168b
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-06-25 16:15:08 +0200

    Finds correct node in tree

--- www/javascript/controllers/project-controller.js
+++ www/javascript/controllers/project-controller.js
@@ -17,7 +17,7 @@ angular.module('main')
 				} else {
 					return false;
 				}
-			}
+			};
 		});
 		$scope.saveProject = function() {
 			$http({method:'POST', url:'/addChild', data: $scope.project}).success(function() {
@@ -25,7 +25,7 @@ angular.module('main')
 			});
 		}
 		$scope.addRootNode = function() {
-			$scope.project.children.push({id: 'node', title:"Root Node" ,nodes: [],collapsed : false});
+			$scope.project.children.push({id: 'node', title:"Root Node" ,nodes: [],collapsed : false, users : [], estimations : []});
 		}
 		$scope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
 			// Appending dialog to document.body to cover sidenav in docs app
@@ -75,7 +75,9 @@ angular.module('main')
 				id: nodeData.id * 10 + nodeData.nodes.length,
 				title: nodeData.title + '.' + (nodeData.nodes.length + 1),
 				nodes: [],
-				collapsed : false
+				collapsed : false,
+				users : [],
+				estimations : []
 			});
 		};
 
@@ -85,5 +87,42 @@ angular.module('main')
 
 		$scope.expandAll = function() {
 			$scope.$broadcast('expandAll');
+		};
+
+		$scope.estimate = function(node) {
+			// $http({method:'POST', url:'/estimate', data: $scope.project}).success(function() {
+
+			// });
+
+			var currnode = $scope.project.children[0];
+			// console.log(currnode);
+			var tmp;
+			$scope.searchTree([currnode], node.$$hashKey, function(res) {
+				tmp = res;
+			});
+
+			console.log(tmp);
+		};
+
+		$scope.searchTree = function(node, id, callback) {
+			var found = false;
+			var result;
+			for (var i in node) {
+				if (id === node[i].$$hashKey) {
+					found = true;
+					callback(node[i]);
+				}
+			}
+			if (found === false) {
+				for (i in node) {
+					$scope.searchTree(node[i].nodes, id, callback);
+				}
+			}
+
+			// callback(result);
+		};
+
+		$scope.getEstimateForUser = function() {
+			return 1;
 		};
 	}]);
