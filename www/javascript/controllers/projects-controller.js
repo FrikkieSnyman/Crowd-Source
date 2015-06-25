5aba4da2f40943cfdc679f5f2b4b511de2bed828
Chris Cloete <u13029721@tuks.co.za>
2015-06-25 12:32:37 +0200

    fix bugs

--- www/javascript/controllers/projects-controller.js
+++ www/javascript/controllers/projects-controller.js
@@ -1,5 +1,5 @@
 angular.module('main')
-.controller('projectsCtrl', ['$scope', '$http', '$location', '$mdDialog', function($scope, $http, $location, $mdDialog) {
+.controller('projectsCtrl', ['$scope', '$http', '$location', '$mdDialog', '$timeout', function($scope, $http, $location, $mdDialog, $timeout) {
 	$http({method:'GET', url:'/projects'}).success(function(data) {
 		$scope.projects = data;
 	});
@@ -9,8 +9,7 @@ angular.module('main')
 		$location.path('/createProject');
 	};
 
-	$scope.deleteProject = function(project) {
-
+	$scope.deleteProject = function(project, ev) {
 		var confirm = $mdDialog.confirm()
 		.parent(angular.element(document.body))
 		.title('Are you sure you wish to delete project ' + project.name + '?')
@@ -18,15 +17,13 @@ angular.module('main')
 		.ariaLabel('Delete the project')
 		.ok('Yes')
 		.cancel('No');
-		$mdDialog.show(confirm).then(function() {
-			$http({method:'POST', url:'/deleteProject', data: project}).success(function() {
-				$scope.updateProjects();
-			});
-		}, function() {
-		});
-
-	};
-
+		$mdDialog.show(confirm).then($timeout(function() {
+				$http({method:'POST', url:'/deleteProject', data: project}).success(function() {
+					$scope.updateProjects();
+				});
+				//debugger;
+			}, function() {}))
+	}
 	$scope.updateProjects = function() {
 		$http({method:'GET', url:'/projects'}).success(function(data) {
 			$scope.projects = data;
