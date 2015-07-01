786799959451bd900c51d888ee6dc1dc892a228c
Chris Cloete <u13029721@tuks.co.za>
2015-07-01 14:42:14 +0200

    trying to fix

--- www/javascript/controllers/project-create-controller.js
+++ www/javascript/controllers/project-create-controller.js
@@ -9,12 +9,22 @@ angular.module('main')
 		var project = {'heading': $scope.projectName, 'description': $scope.description, 'owner' : $rootScope.currentUser, 'users' : $scope.selected};
 		$http({method:'POST', url:'/createProject', data: project}).success(function(res) {
 			if (res === false) {
-				$mdToast.show(
-				$mdToast.simple()
-				.content('Need to be logged in to create a project')
-				.position($scope.getToastPosition())
-				.hideDelay(3000)
-				);
+				if (project.owner === '') {
+					$mdToast.show(
+					$mdToast.simple()
+					.content('Need to be logged in to create a project')
+					.position($scope.getToastPosition())
+					.hideDelay(3000)
+					);
+				} else {
+					$mdToast.show(
+					$mdToast.simple()
+					.content('There already exists a project with that name')
+					.position($scope.getToastPosition())
+					.hideDelay(3000)
+					);
+				}
+
 			} else {
 				$mdToast.show(
 				$mdToast.simple()
@@ -39,13 +49,16 @@ angular.module('main')
 	};
 
 	$scope.selected = [];
-	$scope.toggle = function (item, list) {
-        var idx = list.indexOf(item);
-        if (idx > -1) list.splice(idx, 1);
-        else list.push(item);
-    };
-    
-    $scope.exists = function (item, list) {
-        return list.indexOf(item) > -1;
-    };
+	$scope.toggle = function(item, list) {
+		var idx = list.indexOf(item);
+		if (idx > -1) {
+			list.splice(idx, 1);
+		} else {
+			list.push(item);
+		}
+	};
+
+	$scope.exists = function(item, list) {
+		return list.indexOf(item) > -1;
+	};
 }]);
