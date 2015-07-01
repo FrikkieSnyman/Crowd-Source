7823493412bfbd15e670679c5a09117bc18deec5
cbones1 <bones.greyvenstein@gmail.com>
2015-07-01 12:11:02 +0200

    Continued work

--- www/javascript/controllers/project-controller.js
+++ www/javascript/controllers/project-controller.js
@@ -195,14 +195,10 @@ angular.module('main')
 	}]);
 
 function DialogController($scope, $mdDialog) {
-  $scope.hide = function() {
-    $mdDialog.hide();
-  };
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
-
-  $scope.printToConsole = function(input) {
-  	console.log(input);
-  };
+	$scope.hide = function() {
+		$mdDialog.hide();
+	};
+	$scope.cancel = function() {
+		$mdDialog.cancel();
+	};
 }
