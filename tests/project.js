9bb5f05aa81c7425d4c6b2a8bd3cd5f036b1b768
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-07-06 13:01:18 +0200

    Tests for create project written

--- tests/project.js
+++ tests/project.js
@@ -16,21 +16,49 @@ var result;
 // Mocks
 var request = Object;
 var response = Object;
-response.send = function() {};
+response.res = '';
+response.send = function(res) {response.res = res;};
 
 // Tests
 exports.testCreateProject = function(test) {
-	test.expect(2);
-	request.body = {'heading':'testHeading', 'description':'testDesc'};
+	test.expect(8);
 
+	// Object to create
+	request.body = {'heading':'testHeading', 'description':'testDesc', 'owner': 'testOwner', 'users': ['testUser1', 'testUser2', 'testUser3']};
+
+	// Function being tested
 	testProject.createProject(app, mongoose, request, response);
 
 	expected = 'testHeading';
-	result = response.body.heading;
+	result = response.res.name;
 	test.equal(expected, result);
 
 	expected = 'testDesc';
-	result = response.body.description;
+	result = response.res.description;
+	test.equal(expected, result);
+
+	expected = false;
+	result = response.res.deleted;
+	test.equal(expected, result);
+
+	expected = 0;
+	result = response.res.children.length;
+	test.equal(expected, result);
+
+	expected = 'testOwner';
+	result = response.res.owner;
+	test.equal(expected, result);
+
+	expected = 3;
+	result = response.res.users.length;
+	test.equal(expected, result);
+
+	expected = 'testUser1';
+	result = response.res.users[0];
+	test.equal(expected, result);
+
+	expected = 'testUser3';
+	result = response.res.users[2];
 	test.equal(expected, result);
 
 	test.done();
