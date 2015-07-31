d025f3175e34b3c1895a661a4efaa040aa05998a
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-07-31 12:03:44 +0200

    Uncommenting code

--- app/controllers/notification.server.controller.js
+++ app/controllers/notification.server.controller.js
@@ -50,59 +50,59 @@ exports.sendReport = function(string, project, res) {
 };
 
 exports.sendInvites = function(req, res, next) {
-	// Project.findOne({_id: req.body.projectId}, function(err, project) {
-	// 	if (err) {
-	// 		return res.status(400).send({
-	// 			message: errorHandler.getErrorMessage(err)
-	// 		});
-	// 	} else {
-	// 		var userName;
+	Project.findOne({_id: req.body.projectId}, function(err, project) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			var userName;
 
-	// 		for (var i = project.users.length - 1; i >= 0; i--) {
-	// 			userName = project.users[i];
+			for (var i = project.users.length - 1; i >= 0; i--) {
+				userName = project.users[i];
 
-	// 			User.findOne({username: userName}, function(err, user) {
-	// 				if (err) {} else {
-	// 					async.waterfall([
-	// 						function(done) {
-	// 							res.render('templates/invite-email', {
-	// 								name: user.displayName,
-	// 								appName: config.app.title,
-	// 								projectName: project.name,
-	// 								url: 'http://' + req.headers.host + '/#!/projects/' + project._id + '/edit'
-	// 							}, function(err, emailHTML) {
-	// 								done(err, emailHTML, user);
-	// 							});
-	// 						},
-	// 						// If valid email, send reset email using service
-	// 						function(emailHTML, user, done) {
-	// 							var smtpTransport = nodemailer.createTransport(config.mailer.options);
-	// 							var mailOptions = {
-	// 								to: user.email,
-	// 								from: config.mailer.from,
-	// 								subject: 'Estimation Invite',
-	// 								html: emailHTML
-	// 							};
-	// 							smtpTransport.sendMail(mailOptions, function(err) {
-	// 								if (!err) {
-	// 									// res.send({
-	// 									// 	message: 'An email has been sent to ' + user.email + ' with further instructions.'
-	// 									// });
-	// 								}
+				User.findOne({username: userName}, function(err, user) {
+					if (err) {} else {
+						async.waterfall([
+							function(done) {
+								res.render('templates/invite-email', {
+									name: user.displayName,
+									appName: config.app.title,
+									projectName: project.name,
+									url: 'http://' + req.headers.host + '/#!/projects/' + project._id + '/edit'
+								}, function(err, emailHTML) {
+									done(err, emailHTML, user);
+								});
+							},
+							// If valid email, send reset email using service
+							function(emailHTML, user, done) {
+								var smtpTransport = nodemailer.createTransport(config.mailer.options);
+								var mailOptions = {
+									to: user.email,
+									from: config.mailer.from,
+									subject: 'Estimation Invite',
+									html: emailHTML
+								};
+								smtpTransport.sendMail(mailOptions, function(err) {
+									if (!err) {
+										// res.send({
+										// 	message: 'An email has been sent to ' + user.email + ' with further instructions.'
+										// });
+									}
 
-	// 								done(err);
-	// 							});
-	// 						}
-	// 					], function(err) {
-	// 						if (err) return next(err);
-	// 					});
-	// 				}
-	// 			});
-	// 		};
+									done(err);
+								});
+							}
+						], function(err) {
+							if (err) return next(err);
+						});
+					}
+				});
+			};
 
-	// 		// res.jsonp(projects);
-	// 	}
-	// });
+			// res.jsonp(projects);
+		}
+	});
 
 	// var user;
 
