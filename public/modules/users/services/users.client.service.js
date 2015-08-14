153425fa91e6eb01773b3e7b21b39a7a710e925e
Hanrich <Hanrichp@gmail.com>
2015-08-14 21:25:12 +0200

    the menu bars are now fixed

--- public/modules/users/services/users.client.service.js
+++ public/modules/users/services/users.client.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Users service used for communicating with the users REST endpoint
-angular.module('users').factory('Users', ['$resource','RESOURCE_DOMAIN',
+angular.module('users').factory('Users', ['$resource', 'RESOURCE_DOMAIN',
 	function($resource, RESOURCE_DOMAIN) {
 		return $resource(RESOURCE_DOMAIN + '/users', {}, {
 			update: {
