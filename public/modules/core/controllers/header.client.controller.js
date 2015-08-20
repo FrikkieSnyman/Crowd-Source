6d97807dcb15bf00bc0954b45d7c1fa235248180
Andre Calitz <andcalitz@gmail.com>
2015-08-20 19:06:58 +0200

    Work on the header

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,10 +1,41 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$location', '$mdSidenav', '$mdUtil',
-	function($scope, Authentication, Menus, $location, $mdSidenav, $mdUtil) {
+angular.module('core').controller('HeaderController', ['$scope', '$http', 'Authentication', 'Menus', '$location', '$mdSidenav', '$mdUtil', 'RESOURCE_DOMAIN',
+	function($scope, $http, Authentication, Menus, $location, $mdSidenav, $mdUtil, RESOURCE_DOMAIN) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+
+		$scope.getHeaderPath = function() {
+			var headerPath = $location.$$path;
+
+			if (headerPath === "/") {
+				headerPath = "Welcome";
+			} else if (headerPath.indexOf("edit") !== -1 && headerPath.indexOf("projects")) {
+				var projectID = headerPath.substring(headerPath.indexOf("projects") + 9, headerPath.indexOf("edit") - 1);
+
+				
+			} else {
+				var tokens = headerPath.split("/");
+				headerPath = "";
+
+				for (var i = 0; i < tokens.length; i++) {
+					tokens[i] = capitalizeFirstLetter(tokens[i]);
+
+					headerPath += tokens[i];
+					headerPath += " > ";
+				};
+
+				//Remove the first instance of >
+				headerPath = headerPath.substring(headerPath.indexOf(">") + 1, headerPath.length - 3);
+			}
+
+			return headerPath.trim();
+		}
+
+		var capitalizeFirstLetter = function(string) {
+			return string.charAt(0).toUpperCase() + string.slice(1);
+		}
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
