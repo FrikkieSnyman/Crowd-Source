6766641ed15e99517a8eee4b8f25fc9204c6e86b
Andre Calitz <andcalitz@gmail.com>
2015-08-20 17:15:50 +0200

    Update to the design of the create project page

--- public/modules/projects/controllers/create-project.client.controller.js
+++ public/modules/projects/controllers/create-project.client.controller.js
@@ -7,30 +7,32 @@ angular.module('projects').controller('CreateProjectController', ['$scope', '$st
 
 		$http.get(RESOURCE_DOMAIN+'/users/getUsers').success(function(users) {
 			for (var i in users) {
-				$scope.people.push(users[i].username);
+				$scope.people.push({
+					name: users[i].username,
+					selected: false
+				});
 			}
 		});
 
-		$scope.selected = [];
-		$scope.toggle = function(item, list) {
-			var idx = list.indexOf(item);
-			if (idx > -1) {
-				list.splice(idx, 1);
-			} else {
-				list.push(item);
-			}
-		};
+		var buildSelectedArray = function() {
+			var selected = []
 
-		$scope.exists = function(item, list) {
-			return list.indexOf(item) > -1;
-		};
+			for (var i in $scope.people) {
+				if ($scope.people[i].selected) {
+					selected.push($scope.people[i].name);
+				}
+			}
+
+			return selected;
+		}
 
 		$scope.createProject = function() {
 			//		var project = {'name': $scope.projectName, 'description': $scope.description, 'owner' : Authentication.user, 'users' : $scope.selected};
+
 			var project = new Projects ({
 				name: $scope.projectName,
 				description: $scope.description,
-				users : $scope.selected,
+				users : buildSelectedArray(),
 				owner : $scope.authentication.user.username,
 				openForEstimation : false
 			});
