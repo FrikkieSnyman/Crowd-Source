2418f307c311ae807577511380f946902f9f5eb8
HanrichPotgieter <hanrichp@gmail.com>
2015-08-20 21:02:24 +0200

    Changes to html

--- public/modules/reports/directives/d3-bars.client.directive.js
+++ public/modules/reports/directives/d3-bars.client.directive.js
@@ -58,8 +58,9 @@ angular.module('reports').directive('d3Bars', ['D3', '$window',
 							if (!data) {
 								return;
 							}
-							var width = (d3.select(element[0]).node().offsetWidth - margin);
-							console.log(data);
+
+							var width = d3.select("#chart").node().getBoundingClientRect().width
+							//var width = 400;
 							var height = scope.data.length * (barHeight + barPadding);
 							var color = d3.scale.category20();
 							var xScale = d3.scale.linear()
