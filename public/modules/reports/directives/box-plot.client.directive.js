a115ee092f5cacec91e1da376f8531d31133b62c
HanrichPotgieter <hanrichp@gmail.com>
2015-08-20 20:56:27 +0200

    added some text to the box plot

--- public/modules/reports/directives/box-plot.client.directive.js
+++ public/modules/reports/directives/box-plot.client.directive.js
@@ -15,19 +15,25 @@ angular.module('reports').directive('boxPlot', ['D3', '$window',
 						//.attr('height', 50);
 						//d3.select("body").node().getBoundingClientRect().width
 						var bodyWidth = body.node().getBoundingClientRect().width;
-						var bodyHeight = 50;
+						var bodyHeight = 30;
 						var body = d3.select(element[0]);
 						body.append('p').text('Box plot!');
 
 						var createBox = function(_range, _minOutlier, _minStdDeviation, _median, _maxStdDeviation, _maxOutlier) {
+							var strokeWidth = 2;
+							var bar = body.append('svg')
+							.attr('width', bodyWidth + strokeWidth)
+							.attr('height', bodyHeight + strokeWidth);
+
 							var svg = body.append('svg')
-							.attr('width', bodyWidth)
-							.attr('height', bodyHeight);
+							.attr('width', bodyWidth + strokeWidth)
+							.attr('height', bodyHeight + strokeWidth);
+
 							var middleHeight = (bodyHeight / 2);
 							var middleWidht = (bodyWidth / 2);
 							var range = _range;
 							var ratio = parseInt(bodyWidth / range);
-							var strokeWidth = 30;
+							var strokeWidth = 2;
 							var minOutlier = _minOutlier * ratio;
 							var minStdDeviation = _minStdDeviation * ratio;
 							var median = _median * ratio;
@@ -73,7 +79,7 @@ angular.module('reports').directive('boxPlot', ['D3', '$window',
 							
 							var box = svg.append('rect')
 							.attr('width', maxStdDeviation - minStdDeviation)
-							.attr('height', boxHeight)
+							.attr('height', boxHeight - strokeWidth)
 							.attr('x', minStdDeviation)
 							.attr('y', (bodyHeight / 2) - (boxHeight / 2))
 							.attr('rx', 5)
@@ -86,17 +92,44 @@ angular.module('reports').directive('boxPlot', ['D3', '$window',
 							*/
 							svg.append('line')
 							.attr('x1', median)
-							.attr('y1', middleHeight + boxUpDownLength)
+							.attr('y1', middleHeight + boxUpDownLength - strokeWidth)
 							.attr('x2', median)
-							.attr('y2', middleHeight - boxUpDownLength)
+							.attr('y2', middleHeight - boxUpDownLength - strokeWidth)
 							.style('stroke', 'black');
+
+							/*
+							Creating the
+							*/
+							bar.append('rect')
+							.attr('width', maxOutlier)
+							.attr('height', boxHeight)
+							.attr('x', 0)
+							.attr('y', 0)
+							.attr('rx', 5)
+							.attr('rx', 5)
+							.style('fill', 'lightblue')
+							.style('stroke', 'black');
+
+							bar.append('text')         // append text
+						    .style('fill', 'black')   // fill the text with the colour black
+						    .attr('x', 10)           // set x position of left side of text
+						    .attr('y', middleHeight+5)           // set y position of bottom of text 
+						    .text("Total Units: " + _maxOutlier);
+
+						    bar.append('text')         // append text
+						    .style('fill', 'black')   // fill the text with the colour black
+						    .attr('x', median)           // set x position of left side of text
+						    .attr('y', middleHeight+5)           // set y position of bottom of text 
+						    .text("Median: " + parseInt(_median));
 						};
 
-						createBox(100, 10, 20, 30, 40, 50);
-						createBox(100, 20, 30, 40, 50, 60);
-						createBox(200, 30, 40, 50, 60, 70);
-
-						var visit = function(node, project, data) {
+						//createBox(100, 10, 20, 30, 40, 50);
+						//createBox(100, 20, 30, 40, 50, 60);
+						//createBox(200, 30, 40, 50, 60, 70);
+						//
+						var maxRange = 0;
+						var minRange = 999999999;
+						var visitRange = function(node, project, data) {
 							for (var i = 0; i < node.estimations.length; ++i) {
 								var estimationMean = parseFloat((parseInt(node.minestimations[i]) + 4 * parseInt(node.estimations[i]) + parseInt(node.maxestimations[i])) / 6).toFixed(2);
 								var stdDeviation = parseFloat((parseInt(node.minestimations[i]) - parseInt(node.maxestimations[i])) / 6).toFixed(2);
@@ -115,29 +148,58 @@ angular.module('reports').directive('boxPlot', ['D3', '$window',
 								} else {
 									maxOutlier = parseFloat(maxStdDeviation);
 								}
-								console.log(40 + '}{' + minOutlier + '}{' + minStdDeviation + '}{' + estimationMean + '}{' + maxStdDeviation + '}{' + maxOutlier);
-								createBox(50, minOutlier, minStdDeviation, estimationMean, maxStdDeviation, maxOutlier);
+								if (minOutlier < minRange) {
+									minRange = minOutlier;
+								}
+								if (maxOutlier > maxRange) {
+									maxRange = maxOutlier;		
+								}
+								
 							}
 						};
 
-						var traverseTree = function(node, project, data) {
+						var visitCalc = function(node, project, data) {
+							for (var i = 0; i < node.estimations.length; ++i) {
+								var estimationMean = parseFloat((parseInt(node.minestimations[i]) + 4 * parseInt(node.estimations[i]) + parseInt(node.maxestimations[i])) / 6).toFixed(2);
+								var stdDeviation = parseFloat((parseInt(node.minestimations[i]) - parseInt(node.maxestimations[i])) / 6).toFixed(2);
+
+								var minOutlier;
+								var minStdDeviation = parseFloat((parseFloat(estimationMean) + parseFloat(stdDeviation)));
+								if (node.minestimations[i] < minStdDeviation) {
+									minOutlier = parseInt(node.minestimations[i]);
+								} else {
+									minOutlier = parseFloat(minStdDeviation);
+								}
+								var maxOutlier;
+								var maxStdDeviation = parseFloat((parseFloat(estimationMean) - parseFloat(stdDeviation)));
+								if (node.maxestimations[i] > maxStdDeviation) {
+									maxOutlier = parseInt(node.maxestimations[i]);
+								} else {
+									maxOutlier = parseFloat(maxStdDeviation);
+								}
+								//console.log(40 + '}{' + minOutlier + '}{' + minStdDeviation + '}{' + estimationMean + '}{' + maxStdDeviation + '}{' + maxOutlier);
+								createBox(maxRange - minRange, minOutlier - minRange, minStdDeviation - minRange, estimationMean - minRange, maxStdDeviation - minRange, maxOutlier - minRange);
+							}
+						};
+
+						var traverseTree = function(node, project, data, visit) {
 							if (node === null) {
 								return;
 							}
 							visit(node, project, data);
 							for (var i = 0; i < node.nodes.length; ++i) {
-								traverseTree(node.nodes[i], project, data);
+								traverseTree(node.nodes[i], project, data, visit);
 							}
 						};
 
-						var generateReport = function(project, data) {
+						var generateReport = function(project, data , visit) {
 							var projectTree = project.children[0];
-							traverseTree(projectTree, project, data);
+							traverseTree(projectTree, project, data, visit);
 						};
 
 						scope.data = [];
-
-						generateReport(project, scope.data);
+						generateReport(project, scope.data, visitRange);
+						generateReport(project, scope.data, visitCalc);
 
 						/*
 						var circle = svg.append('circle')
