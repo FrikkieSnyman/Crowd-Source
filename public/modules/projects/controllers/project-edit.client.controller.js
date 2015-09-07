73aa2200823d90b2053b3c9769bd30e43bc441ec
cbones1 <bones.greyvenstein@gmail.com>
2015-09-07 21:07:47 +0200

    Intermediary fix for users on empty project

--- public/modules/projects/controllers/project-edit.client.controller.js
+++ public/modules/projects/controllers/project-edit.client.controller.js
@@ -316,7 +316,6 @@ angular.module('projects').controller('ProjectEditController', ['$scope', '$stat
 				} else {
 					$scope.estimated = true;
 				}
-				$scope.initUsers($scope);
 			});
 		};
 
