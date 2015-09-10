c5e9b68ed8d1cf1cc50af580618d5763d8ea357a
HanrichPotgieter <hanrichp@gmail.com>
2015-09-10 10:05:02 +0200

    Estiamtions

--- public/modules/reports/directives/normal-plot.client.directive.js
+++ public/modules/reports/directives/normal-plot.client.directive.js
@@ -9,21 +9,40 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 				D3.d3().then(function(d3) {
 					scope.$parent.report.$promise.then(function() {
 						var project = scope.$parent.report.project;
-						
-						//setting up empty data array
 						var data = [];
 						
+						function getData() {
+						
+						// loop to populate data array with 
+						// probabily - quantile pairs
+						for (var i = 0; i < 100000; i++) {
+							var q = normal(); // calc random draw from normal dist
+							var p = gaussian(q); // calc prob of rand draw
+							var el = {
+								'q': q,
+								'p': p
+							};
+							data.push(el);
+						}
+						
+						// need to sort for plotting
+						//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+						data.sort(function(x, y) {
+							return x.q - y.q;
+						});	
+						}
+
 						getData(); // popuate data 
 						
 						// line chart based on http://bl.ocks.org/mbostock/3883245
 						var margin = {
-							top: 20,
-							right: 20,
-							bottom: 30,
-							left: 50
-						},
-						width = 960 - margin.left - margin.right,
-						height = 500 - margin.top - margin.bottom;
+								top: 20,
+								right: 20,
+								bottom: 30,
+								left: 50
+							},
+							width = 960 - margin.left - margin.right,
+							height = 500 - margin.top - margin.bottom;
 						
 						var x = d3.scale.linear()
 							.range([0, width]);
@@ -33,11 +52,11 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 						
 						var xAxis = d3.svg.axis()
 							.scale(x)
-							.orient("bottom");
+							.orient('bottom');
 						
 						var yAxis = d3.svg.axis()
 							.scale(y)
-							.orient("left");
+							.orient('left');
 						
 						var line = d3.svg.line()
 							.x(function(d) {
@@ -47,11 +66,11 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 								return y(d.p);
 							});
 						
-						var svg = d3.select(element[0]).append("svg")
-							.attr("width", width + margin.left + margin.right)
-							.attr("height", height + margin.top + margin.bottom)
-							.append("g")
-							.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+						var svg = d3.select(element[0]).append('svg')
+							.attr('width', width + margin.left + margin.right)
+							.attr('height', height + margin.top + margin.bottom)
+							.append('g')
+							.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 						
 						x.domain(d3.extent(data, function(d) {
 							return d.q;
@@ -60,40 +79,19 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 							return d.p;
 						}));
 						
-						svg.append("g")
-							.attr("class", "x axis")
-							.attr("transform", "translate(0," + height + ")")
+						svg.append('g')
+							.attr('class', 'x axis')
+							.attr('transform', 'translate(0,' + height + ')')
 							.call(xAxis);
 						
-						svg.append("g")
-							.attr("class", "y axis")
+						svg.append('g')
+							.attr('class', 'y axis')
 							.call(yAxis);
 						
-						svg.append("path")
+						svg.append('path')
 							.datum(data)
-							.attr("class", "line")
-							.attr("d", line);
-						
-						function getData() {
-						
-						// loop to populate data array with 
-						// probabily - quantile pairs
-						for (var i = 0; i < 100000; i++) {
-							var q = normal() // calc random draw from normal dist
-							var p = gaussian(q) // calc prob of rand draw
-							var el = {
-								"q": q,
-								"p": p
-							}
-							data.push(el)
-						};
-						
-						// need to sort for plotting
-						//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-						data.sort(function(x, y) {
-							return x.q - y.q;
-						});	
-						}
+							.attr('class', 'line')
+							.attr('d', line);
 						
 						// from http://bl.ocks.org/mbostock/4349187
 						// Sample from a normal distribution with mean 0, stddev 1.
@@ -105,7 +103,7 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 								x = Math.random() * 2 - 1;
 								y = Math.random() * 2 - 1;
 								rds = x * x + y * y;
-							} while (rds == 0 || rds > 1);
+							} while (rds === 0 || rds > 1);
 							c = Math.sqrt(-2 * Math.log(rds) / rds); // Box-Muller transform
 							return x * c; // throw away extra sample y * c
 						}
@@ -118,9 +116,8 @@ angular.module('reports').directive('normalPlot', ['D3', '$window',
 								sigma = 1;
 						
 							x = (x - mean) / sigma;
-							return gaussianConstant * Math.exp(-.5 * x * x) / sigma;
-						};
-						
+							return gaussianConstant * Math.exp(-0.5 * x * x) / sigma;
+						}
 					});
 				});
 			}
