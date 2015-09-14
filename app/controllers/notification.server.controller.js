4a171432edb6f8ee771c10b8c9482d57dc4debef
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-09-14 16:07:18 +0200

    Socketio integrated. Works for estimators estimating at same time

--- app/controllers/notification.server.controller.js
+++ app/controllers/notification.server.controller.js
@@ -13,101 +13,101 @@ var mongoose = require('mongoose'),
 	_ = require('lodash');
 
 exports.sendInvites = function(req, res, next) {
-	Project.findOne({_id: req.body.projectId}, function(err, project) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			var userName;
+	// Project.findOne({_id: req.body.projectId}, function(err, project) {
+	// 	if (err) {
+	// 		return res.status(400).send({
+	// 			message: errorHandler.getErrorMessage(err)
+	// 		});
+	// 	} else {
+	// 		var userName;
 			
-			var funcDone = function(user, done) {
-				res.render('templates/invite-email', {
-					name: user.displayName,
-					//name: 'No_Name',
-					appName: config.app.title,
-					projectName: project.name,
-					url: 'http://' + req.headers.host + '/#!/projects/' + project._id + '/edit'
-				}, function(err, emailHTML) {
-					done(err, emailHTML, user);
-				});
-			};
+	// 		var funcDone = function(user, done) {
+	// 			res.render('templates/invite-email', {
+	// 				name: user.displayName,
+	// 				//name: 'No_Name',
+	// 				appName: config.app.title,
+	// 				projectName: project.name,
+	// 				url: 'http://' + req.headers.host + '/#!/projects/' + project._id + '/edit'
+	// 			}, function(err, emailHTML) {
+	// 				done(err, emailHTML, user);
+	// 			});
+	// 		};
 			
-			var funcEmail = function(emailHTML, user, done) {
-				var smtpTransport = nodemailer.createTransport(config.mailer.options);
-				var mailOptions = {
-					to: user.email,
-					from: config.mailer.from,
-					subject: 'Estimation Invite',
-					html: emailHTML
-				};
-				smtpTransport.sendMail(mailOptions, function(err) {
-					if (!err) {
-						console.log('No Error.');
-					}
+	// 		var funcEmail = function(emailHTML, user, done) {
+	// 			var smtpTransport = nodemailer.createTransport(config.mailer.options);
+	// 			var mailOptions = {
+	// 				to: user.email,
+	// 				from: config.mailer.from,
+	// 				subject: 'Estimation Invite',
+	// 				html: emailHTML
+	// 			};
+	// 			smtpTransport.sendMail(mailOptions, function(err) {
+	// 				if (!err) {
+	// 					console.log('No Error.');
+	// 				}
 
-					done(err, user);
-				});
-			};
+	// 				done(err, user);
+	// 			});
+	// 		};
 			
-			var funcError = function(err) {
-				if (err) {
-					return next(err);
-				}
-			};
+	// 		var funcError = function(err) {
+	// 			if (err) {
+	// 				return next(err);
+	// 			}
+	// 		};
 			
-			var funcPromise = function(err, user) {
-				if (!err) {
-					async.waterfall([
-						async.apply(funcDone,user),
-						// If valid email, send reset email using service
-						funcEmail
-					],funcError );
-				}
-			};
+	// 		var funcPromise = function(err, user) {
+	// 			if (!err) {
+	// 				async.waterfall([
+	// 					async.apply(funcDone,user),
+	// 					// If valid email, send reset email using service
+	// 					funcEmail
+	// 				],funcError );
+	// 			}
+	// 		};
 
-			for (var i = project.users.length - 1; i >= 0; i--) {
-				userName = project.users[i];
+	// 		for (var i = project.users.length - 1; i >= 0; i--) {
+	// 			userName = project.users[i];
 
-				User.findOne({username: userName}, funcPromise);
-			}
-		}
-	});
+	// 			User.findOne({username: userName}, funcPromise);
+	// 		}
+	// 	}
+	// });
 };
 exports.sendReport = function(string, project, res) {
-	var owner = project.owner;
-	User.findOne({username : owner}, function(err, user) {
-		if (!err) {
-			async.waterfall([
-				function(done) {
-					res.render('templates/report-email', {
-						name: user.displayName,
-						appName: config.app.title,
-						projectName: project.name,
-						report: string
-					}, function(err, emailHTML) {
-						done(err, emailHTML, user);
-					});
-				},
-				function(emailHTML, user, done) {
-					var smtpTransport = nodemailer.createTransport(config.mailer.options);
-					var mailOptions = {
-						to: user.email,
-						from: config.mailer.from,
-						subject: 'Estimation report for ' + project.name,
-						html: emailHTML
-					};
-					smtpTransport.sendMail(mailOptions, function(err) {
-						if (!err) {
-							console.log('No error');
-						}
-						done(err);
-					});
-				}
-				], function(err) {
-					if (err) return err;
-				});
-		}
-	});
+	// var owner = project.owner;
+	// User.findOne({username : owner}, function(err, user) {
+	// 	if (!err) {
+	// 		async.waterfall([
+	// 			function(done) {
+	// 				res.render('templates/report-email', {
+	// 					name: user.displayName,
+	// 					appName: config.app.title,
+	// 					projectName: project.name,
+	// 					report: string
+	// 				}, function(err, emailHTML) {
+	// 					done(err, emailHTML, user);
+	// 				});
+	// 			},
+	// 			function(emailHTML, user, done) {
+	// 				var smtpTransport = nodemailer.createTransport(config.mailer.options);
+	// 				var mailOptions = {
+	// 					to: user.email,
+	// 					from: config.mailer.from,
+	// 					subject: 'Estimation report for ' + project.name,
+	// 					html: emailHTML
+	// 				};
+	// 				smtpTransport.sendMail(mailOptions, function(err) {
+	// 					if (!err) {
+	// 						console.log('No error');
+	// 					}
+	// 					done(err);
+	// 				});
+	// 			}
+	// 			], function(err) {
+	// 				if (err) return err;
+	// 			});
+	// 	}
+	// });
 };
 
