3394f465997edb158cc7c44c27b84af05cecef78
HanrichPotgieter <hanrichp@gmail.com>
2015-10-14 11:41:10 +0200

    Fixed linter errors

--- public/modules/projects/tests/project-edit.client.controller.test.js
+++ public/modules/projects/tests/project-edit.client.controller.test.js
@@ -224,37 +224,4 @@
 		
 
 	});
-	var sockMock = function($rootScope){
-	this.events = {};
-	this.emits = {};
-	
-	// intercept 'on' calls and capture the callbacks
-	this.on = function(eventName, callback){
-		if(!this.events[eventName]) this.events[eventName] = [];
-		this.events[eventName].push(callback);
-	};
-	
-	// intercept 'emit' calls from the client and record them to assert against in the test
-	this.emit = function(eventName){
-		var args = Array.prototype.slice.call(arguments, 1);
-	
-		if(!this.emits[eventName])
-		this.emits[eventName] = [];
-		this.emits[eventName].push(args);
-	};
-	
-	//simulate an inbound message to the socket from the server (only called from the test)
-	this.receive = function(eventName){
-		var args = Array.prototype.slice.call(arguments, 1);
-	
-		if(this.events[eventName]){
-		angular.forEach(this.events[eventName], function(callback){
-			$rootScope.$apply(function() {
-			callback.apply(this, args);
-			});
-		});
-		};
-	};
-	
-	};
 }());
