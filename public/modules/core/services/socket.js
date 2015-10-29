76a5f5a5bb50a77532553386d9620537d8fe1d88
Andre Calitz <andcalitz@gmail.com>
2015-10-29 18:08:50 +0200

    Fix for socket io server address

--- public/modules/core/services/socket.js
+++ public/modules/core/services/socket.js
@@ -2,10 +2,10 @@
 
 //socket factory that provides the socket service
 angular.module('core').factory('Socket', ['socketFactory',
-    function(socketFactory) {
-        return socketFactory({
-            prefix: '',
-            ioSocket: io.connect('http://localhost:3000')
-        });
-    }
+	function(socketFactory) {
+		return socketFactory({
+			prefix: '',
+			ioSocket: io.connect()
+		});
+	}
 ]);
